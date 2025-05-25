f3bec946bcd7ab6a946f27fc70ddd0048d232194
anshpateriya1802 <pateriyaansh1802@gmail.com>
2025-05-25 20:03:17 +0530

    title updation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "smartcraze-suraj vishwakarma",
+  title: "E-learningByAnsh",
   description: "E-Learning Platform",
 };
 
